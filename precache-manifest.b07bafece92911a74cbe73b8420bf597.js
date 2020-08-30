6c8f83254f683c5b62f109c88716133994448850
Himanshu Negi <negehimanshu9756@gmail.com>
2020-08-30 13:13:45 +0530

    Updates

---
+++ precache-manifest.b07bafece92911a74cbe73b8420bf597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2d2a2a668ac7b067b76bc9722d8d08",
+    "url": "/Starwisptask/index.html"
+  },
+  {
+    "revision": "a21d0d07e086b33d788a",
+    "url": "/Starwisptask/static/css/main.cb174c62.chunk.css"
+  },
+  {
+    "revision": "968766e38b939a80ac6d",
+    "url": "/Starwisptask/static/js/2.784e0ab0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Starwisptask/static/js/2.784e0ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21d0d07e086b33d788a",
+    "url": "/Starwisptask/static/js/main.93900c67.chunk.js"
+  },
+  {
+    "revision": "a03f4f0d40b3877ecdbd",
+    "url": "/Starwisptask/static/js/runtime-main.ebe7be7e.js"
+  }
+]);
